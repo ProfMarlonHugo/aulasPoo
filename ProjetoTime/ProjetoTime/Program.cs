78cb117440126a0706ebe8598082dface2542fce
Marlon <br.marlon.hugo@gmail.com>
2020-02-21 21:45:49 -0300

    inicio do projeto TIME

---
+++ ProjetoTime/ProjetoTime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Instancia de um objeto*/
+            Time objTime = new Time();
+            objTime.nome = "Palmeiras";
+            objTime.comprarJogador(1);
+
+            /*Mostrar valor para o usuário*/
+            System.Console.Write(@"Entre com a cidade do time "+ objTime.nome + ": ");
+            /*Ler entrada do usuário*/
+            objTime.cidade = System.Console.ReadLine();
+
+            /*Mostrar valor para o usuário*/
+            System.Console.Write(@"Entre com o estado da cidade " + objTime.cidade+ ": ");
+            /*Ler entrada do usuário*/
+            objTime.estado = System.Console.ReadLine();
+
+            /*Mostrar valor para o usuário*/
+            System.Console.Write(@"Entre com o país do estado " + objTime.estado+ ": ");
+            /*Ler entrada do usuário*/
+            objTime.pais = System.Console.ReadLine();
+
+            /*Mostrar valor para o usuário*/
+            System.Console.Write(@"Entre com o número de jogadores do time " + objTime.nome + ": ");
+            /*Ler entrada do usuário*/
+            objTime.numeroDeJogadores = Convert.ToInt32(System.Console.ReadLine());
+            
+            /*Mostrar valor para o usuário*/
+            System.Console.Write(@"Entre com o valor do time " + objTime.nome + ": R$");
+            /*Ler entrada do usuário*/
+            objTime.valorDoTimeEmDinheiro = Convert.ToInt32(System.Console.ReadLine());
+
+            /*Mostrar stado do meu objeto*/
+            string statusDoObjeto;
+            statusDoObjeto = objTime.statusClass();
+            System.Console.WriteLine(statusDoObjeto);
+
+            /*Parar o programa*/
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ ProjetoTime/ProjetoTime/Time.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ProjetoTime
+{
+    class Time
+    {
+        /*Atributos (Características da Classe)*/
+        public string nome;
+        public string pais;
+        public string estado;
+        public string cidade;
+        public int numeroDeJogadores = 0;
+        public int numeroDeTitulos = 0;
+        public double valorDoTimeEmDinheiro = 0;
+
+        /*Métodos */
+        public void comprarJogador(int numJongador)
+        {
+            this.numeroDeJogadores += numJongador;
+        }
+
+        public string statusClass()
+        {
+            string retorno;
+            retorno = "\nNome: "+this.nome +
+                "\nCidade: "+this.cidade + 
+                "\nEstado: "+this.estado +
+                "\nNúmero de Jogadores: "+this.numeroDeJogadores +
+                "\nNúmero de Títulos: "+this.numeroDeTitulos +
+                "\nValor do time: R$"+this.valorDoTimeEmDinheiro;
+
+            return retorno;
+        }
+
+    }
+}
